82e92043f5519418ce4d27817aa105af3d9e9e61
Martin Gran <martgra@gmail.com>
2017-01-27 16:45:29 +0100

    Herro

---
+++ da-nan3000/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=da-nan3000
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/da-nan3000
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=da-nan3000.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/da-nan3000.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=da-nan3000
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/da-nan3000
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=da-nan3000.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/da-nan3000.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
